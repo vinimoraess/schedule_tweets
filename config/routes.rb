f326dcc1fabeb59b869a4767af5bbeb7c4938e56
vinimoraess <vinicius_jag@hotmail.com>
2022-01-10 00:15:27 -0300

    Sign up created

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   #GET /about
   get "about-us", to: "about#index", as: :about
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   root to: "main#index"
 end
